ce5942be1c9889ad2239262708db1579c8cfaa80
Lennox Mwabonje <lenblazy@gmail.com>
2018-10-23 08:41:55 +0300

    Added the about section

---
+++ app/src/main/java/com/example/lennox/flexture/About.java
@@ -0,0 +1,54 @@
+package com.example.lennox.flexture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+
+public class About extends AppCompatActivity {
+
+    Animation anim1, anim2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        moveViewToScreenCenter(findViewById(R.id.dev_header));
+        moveViewToScreenCenter(findViewById(R.id.author1));
+        moveViewToScreenCenter(findViewById(R.id.author2));
+        moveIcon(findViewById(R.id.imageViewAbout));
+    }
+
+    private void moveIcon(View viewById) {
+        int originalPos[] = new int[2];
+        viewById.getLocationOnScreen(originalPos);
+
+        anim2 = new TranslateAnimation(0, 0, 0, originalPos[1] + 100);
+        anim2.setDuration(2000);
+        anim2.setFillAfter(true);
+        viewById.startAnimation(anim2);
+    }
+
+    private void moveViewToScreenCenter(View viewById) {
+            RelativeLayout root = (RelativeLayout) findViewById(R.id.rel_root);
+            DisplayMetrics dm = new DisplayMetrics();
+            this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+            int statusBarOffset = dm.heightPixels - root.getMeasuredHeight();
+
+            int originalPos[] = new int[2];
+            viewById.getLocationOnScreen(originalPos);
+
+            int xDest = dm.widthPixels / 2;
+            xDest -= (viewById.getMeasuredWidth() / 2);
+            int yDest = dm.heightPixels / 2 - (viewById.getMeasuredHeight() / 2) - statusBarOffset;
+
+            anim1 = new TranslateAnimation(0, 0, 0, yDest - originalPos[1] + 250);
+            anim1.setDuration(1500);
+            anim1.setFillAfter(true);
+            viewById.startAnimation(anim1);
+
+    }
+}
--- app/src/main/java/com/example/lennox/flexture/Flexture.java
+++ app/src/main/java/com/example/lennox/flexture/Flexture.java
@@ -1,9 +1,11 @@
 package com.example.lennox.flexture;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -31,13 +33,54 @@ public class Flexture extends AppCompatActivity {
     private void init() {
         activity = this;
         fields = new ArrayList<>();
-        fields.add("ATTENDANCE");
-        fields.add("SCHEDULER");
-        fields.add("NOTES");
-        fields.add("PROFILE");
-        fields.add("CGPA CALCULATOR");
+
+        //check the role
+        Boolean role = getIntent().getBooleanExtra("ROLE", true);
+
+        //true value for students and false value for lecturers
+        if (role == true) {
+            studentActivities();
+        } else {
+            lecturerActivities();
+        }
+
         gridview = (GridView) findViewById(R.id.grid);
         adapter = new GridAdapter(this, fields);
         gridview.setAdapter(adapter);
+    }
+
+    private void lecturerActivities() {
+        fields.add("ATTENDANCE");
+        fields.add("SCHEDULER");
+        fields.add("NOTES");
+        fields.add("CGPA CALCULATOR");
+    }
+
+    private void studentActivities() {
+        fields.add("ATTENDANCE");
+        //fields.add("TIME TABLE");
+        fields.add("NOTES");
+        fields.add("CGPA CALCULATOR");
+        fields.add("PROFILE");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                //open settings page
+                return true;
+            case R.id.about:
+                Intent about = new Intent(this, About.class);
+                startActivity(about);
+                return true;
+            case R.id.logout:
+                //logout user
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 }
